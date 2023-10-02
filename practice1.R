5d703ecf4985a07729760077f8f4d723aad15639
ethen7 <144878772+ethen7@users.noreply.github.com>
2023-10-02 21:06:40 +0800

    Add files via upload

---
+++ practice1.R
@@ -0,0 +1,19 @@
+#Basic Data Type-------
+c("John", "Mary")
+c(2, 3.1412)
+c(TRUE, TRUE, F,T)#Can interchange between T and TURE, F and FALSE
+#using c() to collect your value
+typeof(c("John","Mary"))
+typeof(list("John", 178, TRUE))
+
+#Binding----
+personName=c("John", "Mary")
+interestingNumber=c(2, 3.1412)
+covidPositive=c(TRUE, TRUE, F)
+
+#name call to access value
+personName
+interestingNumber
+covidPositive
+
+personName
